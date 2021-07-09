69171dd7c6bbdb03393e00a7c0eec45de2b1e934
Logan Riffell <lkriffell@gmail.com>
2021-07-09 16:11:42 -0400

    Add enemy movement

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform player;
+
+    public float speed = 4f;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 pos = Vector3.MoveTowards(transform.position, player.position, speed * Time.fixedDeltaTime);
+        transform.LookAt(player);
+        rb.MovePosition(pos);
+
+    }
+}
