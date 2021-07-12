c396ed62ade78bb9745d4036a37e60b99e4f5ed4
Logan Riffell <lkriffell@gmail.com>
2021-07-12 16:43:25 -0400

    Update target

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public float health = 50f;
+    public float health;
+    public bool dead;
+    public AudioSource audio;
+    public AudioClip hitSound;
+
+    void Start()
+    {
+    }
     
     public void TakeDamage (float amount)
     {
+      Debug.Log("it got hit");
       health -= amount;
+      audio.PlayOneShot(hitSound);
       if (health <= 0)
       {
         Die();
@@ -17,6 +26,34 @@ public class Target : MonoBehaviour
 
     void Die()
     {
-      Destroy(gameObject);
+      Debug.Log("it died");
+      gameObject.layer = 10;
+      gameObject.tag = "Untagged";
+      dead = true;
+      GetComponent<Animator>().enabled = false;
+      GetComponent<CharacterAI>().enabled = false;
+      SetRigidbodyState(false);
+      // SetColliderState(true);
+      Destroy(gameObject, 5f);
+    }
+
+    public void SetRigidbodyState(bool state)
+    {
+      Rigidbody[] rigidBodies = GetComponentsInChildren<Rigidbody>();
+      foreach (Rigidbody rigidbody in rigidBodies)
+      {
+        rigidbody.isKinematic = state;
+      }
+      GetComponent<Rigidbody>().isKinematic = !state;
+    }
+
+    public void SetColliderState(bool state)
+    {
+      Collider[] colliders = GetComponentsInChildren<Collider>();
+      foreach (Collider collider in colliders)
+      {
+        collider.enabled = state;
+      }
+      GetComponent<Collider>().enabled = !state;
     }
 }
