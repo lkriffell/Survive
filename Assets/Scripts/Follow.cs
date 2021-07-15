4fa3f71bd9e5d1ff971c28d0dd92b9260ad23ace
Logan Riffell <lkriffell@gmail.com>
2021-07-15 15:05:15 -0400

    Rename followPlayer to follow

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -3,12 +3,12 @@
 public class FollowPlayer : MonoBehaviour
 {
 
-    public Transform player;
+    public Transform objectToFollow;
     public Vector3 offset;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = player.position + offset;
+        transform.position = objectToFollow.position + offset;
     }
 }
