e7ccd31d11bcf46db602d3b2ec9ecbf6df4e8597
Logan Riffell <lkriffell@gmail.com>
2021-07-15 15:21:15 -0400

    Update camera controller so player can fly and look around

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float mouseSensitivity = 200f;
-    public Transform playerBody;
+    public float mouseSensitivity = 100f;
     float xRotation = 0f;
+    float speed = 10f;
+    public Rigidbody rb;
+    private CharacterController controller;
+    private bool inFlyMode;
 
     void Start () {
+      rb = GetComponentInParent<Rigidbody>();
+      controller = GetComponent<CharacterController>();
+      FlyModeOn();
+    }
+
+    void FixedUpdate () {
+      CheckState();
+      if (!inFlyMode) return;
+      Movement();
+      LookRotation();
+    }
+
+    private void FlyModeOn()
+    {
+      inFlyMode = true;
       Cursor.lockState = CursorLockMode.Locked;
       Cursor.visible = false;
     }
 
-    void Update () {
+    private void FlyModeOff()
+    {
+      controller.Move(new Vector3(0, 0, 0));
+      inFlyMode = false;
+      Cursor.lockState = CursorLockMode.None;
+      Cursor.visible = true;
+    }
+
+    private void CheckState()
+    {
+      if (Input.GetKey(KeyCode.Mouse0)) FlyModeOff();
+      if (Input.GetKey(KeyCode.Mouse1)) FlyModeOn();
+    }
+
+    private void LookRotation()
+    {
       float mouseX = Input.GetAxis ("Mouse X") * mouseSensitivity * Time.deltaTime;
       float mouseY = Input.GetAxis ("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -21,6 +54,16 @@ public class CameraController : MonoBehaviour
       xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
       transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-      playerBody.Rotate(Vector3.up * mouseX);
+      rb.transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void Movement()
+    {
+      float x = Input.GetAxis("Horizontal");
+      float z = Input.GetAxis("Vertical");
+
+      Vector3 move = transform.right * x + transform.forward * z;
+
+      controller.Move(move * speed * Time.deltaTime);
     }
 }
