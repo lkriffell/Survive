0e3b131366e5111469c1dda0024fc0b4d8275a70
Logan Riffell <lkriffell@gmail.com>
2021-07-21 17:32:56 -0400

    Add citizen class

--- Assets/Scripts/Citizen.cs
+++ Assets/Scripts/Citizen.cs
@@ -1,25 +1,87 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Citizen : MonoBehaviour
 {
     public int level = 1;
-    public float health;
-    public int maxCarry;
+    public float health = 100;
+    public int maxCarry = 20;
     public int carryingNow = 0;
-    public Dictionary<String, int> _inventory = new Dictionary<String, int>(){{"Wood", 0}, {"Stone", 0}};
-    public GameObject PrimaryRole;
-    public Laborer SecondaryRole;
+    public string _resourceToDeliver = "";
+    public string _resourceToTake = "";
+    public int _amountToTake;
 
-    public void TakeDamage(int amount)
-    {
-        health -= amount;
-    }
+    public bool takeFromStorage;
+    public bool giveToStorage;
+
+    public Dictionary<String, int> _inventory = new Dictionary<String, int>(){{"Wood", 0}, {"Stone", 0}};
+
+    public float _lastSearch = 0f;
+    public GatherableResource ResourceTarget;
+    public Storage StorageTarget;
+    public BuildSite BuildSiteTarget;
+    public Builder PrimaryRole;
+    public Laborer SecondaryRole;
 
     private void Awake()
     {
-        health = 100;
-        maxCarry = 20;
+        if (PrimaryRole != null) SecondaryRole.enabled = false;
+    }
+
+    void FixedUpdate() => _lastSearch += Time.deltaTime;
+
+    public void SetMostResource()
+    {
+      _resourceToDeliver = _inventory.FirstOrDefault(x => x.Value == _inventory.Values.Max()).Key;
+    }
+
+    public bool InventoryFull()
+    {
+      if (carryingNow >= maxCarry) return true;
+      else return false;
+    }
+
+    public void TakeFromResource()
+    {
+      if (!InventoryFull() && ResourceTarget.Take())
+      {
+        if (_inventory.ContainsKey(ResourceTarget.resourceType)) _inventory[ResourceTarget.resourceType]++;
+        else _inventory.Add(ResourceTarget.resourceType, 1);
+        carryingNow++;
+      }
+      else ResourceTarget = null;
+    }
+
+    public void TakeFromStorage()
+    {
+      if (StorageTarget.Take(_resourceToTake))
+      {
+        if (_inventory.ContainsKey(_resourceToTake)) _inventory[_resourceToTake]++;
+        else _inventory.Add(_resourceToTake, 1);
+        carryingNow++;
+      }
+      else StorageTarget = null;
+    }
+
+    public void GiveToStorage(String resourceType)
+    {
+      if (_inventory[resourceType] > 0 && StorageTarget.Give(resourceType))
+      {
+        _inventory[resourceType]--;
+        carryingNow--;
+      }
+      else StorageTarget = null;
+    }
+
+    public void GiveToBuildSite()
+    {
+      if (_inventory[_resourceToTake] > 0 && BuildSiteTarget.Give(_resourceToTake))
+      {
+        _inventory[_resourceToTake]--;
+        carryingNow--;
+      }
+      else BuildSiteTarget = null;
     }
 }
